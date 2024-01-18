d0aab0ffba690b19d2b19f05003e33a02180dcf1
Christian Kobril <christiankobril@gmail.com>
2024-01-18 23:05:10 +0100

    ui: show connect wallet and selected network in navbar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import styled from 'styled-components'
 
+import styles from '../styles/styles.module.css'
+
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
@@ -18,7 +20,106 @@ export function Nav() {
   return (
     <Wrapper>
       <Title>DAO GHO Grants</Title>
-      <ConnectButton chainStatus="none" showBalance={false} />
+      <ConnectButton.Custom>
+        {({
+          account,
+          chain,
+          openAccountModal,
+          openChainModal,
+          openConnectModal,
+          authenticationStatus,
+          mounted,
+        }) => {
+          // Note: If your app doesn't use authentication, you
+          // can remove all 'authenticationStatus' checks
+          const ready = mounted && authenticationStatus !== 'loading'
+          const connected =
+            ready &&
+            account &&
+            chain &&
+            (!authenticationStatus || authenticationStatus === 'authenticated')
+
+          return (
+            <div
+              {...(!ready && {
+                'aria-hidden': true,
+                style: {
+                  opacity: 0,
+                  pointerEvents: 'none',
+                  userSelect: 'none',
+                },
+              })}
+            >
+              {(() => {
+                if (!connected) {
+                  return (
+                    <button
+                      onClick={openConnectModal}
+                      type="button"
+                      className={styles.connectButton}
+                    >
+                      Connect Wallet
+                    </button>
+                  )
+                }
+                if (chain.unsupported) {
+                  return (
+                    <button
+                      onClick={openChainModal}
+                      type="button"
+                      className={styles.connectButton}
+                    >
+                      Wrong network
+                    </button>
+                  )
+                }
+                return (
+                  <div style={{ display: 'flex', gap: 12 }}>
+                    <button
+                      onClick={openChainModal}
+                      style={{ display: 'flex', alignItems: 'center' }}
+                      type="button"
+                      className={styles.connectButton}
+                    >
+                      {chain.hasIcon && (
+                        <div
+                          style={{
+                            background: chain.iconBackground,
+                            width: 20,
+                            height: 20,
+                            borderRadius: 999,
+                            overflow: 'hidden',
+                            marginRight: 4,
+                          }}
+                        >
+                          {chain.iconUrl && (
+                            <img
+                              alt={chain.name ?? 'Chain icon'}
+                              src={chain.iconUrl}
+                              style={{ width: 20, height: 20 }}
+                            />
+                          )}
+                        </div>
+                      )}
+                      {chain.name}
+                    </button>
+                    <button
+                      onClick={openAccountModal}
+                      type="button"
+                      className={styles.connectButton}
+                    >
+                      {account.displayName}
+                      {account.displayBalance
+                        ? ` (${account.displayBalance})`
+                        : ''}
+                    </button>
+                  </div>
+                )
+              })()}
+            </div>
+          )
+        }}
+      </ConnectButton.Custom>
     </Wrapper>
   )
 }
