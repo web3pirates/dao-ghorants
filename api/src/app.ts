36e66e49f5681377cf8845e5eb5992a67ae65d69
Christian Kobril <christiankobril@gmail.com>
2024-01-19 17:24:58 +0100

    chore: change port from 3000 to 8000

--- api/src/app.ts
+++ api/src/app.ts
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const port = 3000;
+const port = 8000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
